be9480d131404db0315236994442036ba40e607a
Valeria Durruty <vale_gelp87@hotmail.com>
2025-07-22 23:37:33 -0300

    Se agrega models de Actores, Tag, Reparto y Contenido_Tag.

--- src/models/trailerflix.js
+++ src/models/trailerflix.js
@@ -106,7 +106,87 @@ Contenido.belongsTo(Genero, {
 Genero.hasMany(Contenido, {
     foreignKey: "genero_id"
 });
+
+//registro de tabla Actores
+const Actores = sequelize.define('Actores', {
+    actores_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nombre_completo: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+}, {
+    tableName: 'actores',
+    timestamps: false
+});
+
+// Asociación con contenido a través de reparto
+Actores.associate = models => {
+    Actores.belongsToMany(models.Contenido, {
+        through: models.Reparto,
+        foreignKey: 'actores_id',
+        otherKey: 'contenido_id'
+    });
+};
+
+//registro de tabla Tag
+const Tag = sequelize.define('Tag', {
+    tag_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+}, {
+    tableName: 'tags',
+    timestamps: false
+});
+
+// Asociación con contenido a través de Contenido_Tag
+Tag.associate = models => {
+    Tag.belongsToMany(models.Contenido, {
+        through: models.Contenido_Tag,
+        foreignKey: 'tag_id',
+        otherKey: 'contenido_id'
+    });
+};
+
+//registro de tabla Reparto
+const Reparto = sequelize.define('Reparto', {
+    contenido_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    actores_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+}, {
+    tableName: 'reparto',
+    timestamps: false
+});
+
+//registro de tabla Contenido_Tag
+const Contenido_Tag = sequelize.define('Contenido_Tag', {
+    contenido_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+}, {
+    tableName: 'contenido_tag',
+    timestamps: false
+});
   
 //exportar los modelos de tabla de contenido, categoria, genero
-module.exports = { Contenido, Categoria, Genero };
+module.exports = { Contenido, Categoria, Genero, Actores, Tag, Reparto, Contenido_Tag };
 
