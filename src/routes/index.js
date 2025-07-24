ed3d715ceb99e07332a8a5e22c75c7d118a3788b
YESICAFINOCCHIO <yesicafinoccho@gmail.com>
2025-07-24 14:53:29 -0300

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,28 @@
 //Definir todas las rutas
-
 const express = require("express");
-
-//metodo router de express
 const router = express.Router();
 
-//controladores de las funciones
+// controladores ya existentes
 const getAll = require("./trailerflix/getAll");
-const getGenero = require("./trailerflix/getGenero")
+const getGenero = require("./trailerflix/getGenero");
 const getResumenMisionRoute = require("./trailerflix/getResumenMision");
 const getPeliculas = require("./trailerflix/getPeliculas");
 const getSeries = require("./trailerflix/getSeries");
 const getChrisPrattJobs = require("./trailerflix/getChrisPrattJobs");
-const contarPelis = require("./trailerflix/contarPelis")
-const contarSeries = require("./trailerflix/contarSeries")
+const contarPelis = require("./trailerflix/contarPelis");
+const contarSeries = require("./trailerflix/contarSeries");
 
+// yesi
+const tagsRouter = require("./trailerflix/tags");
+const temporada = require("./trailerflix/temporada");
+const actores = require("./trailerflix/actores");
+const actoresCant = require("./trailerflix/actores_cant");
+const seriesOrden = require("./trailerflix/series_orden");
+const fechaLanzamiento = require("./trailerflix/fecha_lanzamiento");
+const buscar = require('./trailerflix/buscar');
+const rankingRouter = require('./trailerflix/ranking');
 
-//endpoints
+// endpoints
 router.get("/trailerflix", getAll);
 router.use("/contenido", getGenero);
 router.use("/resumen-mision", getResumenMisionRoute);
@@ -25,5 +31,15 @@ router.use("/series", getSeries);
 router.use("/chris-pratt-jobs", getChrisPrattJobs);
 router.use("/", contarPelis);
 router.use("/", contarSeries);
+
+// yesi
+router.use("/", tagsRouter);  
+router.use("/", temporada);
+router.use("/", actores);
+router.use("/", actoresCant);
+router.use("/", seriesOrden);
+router.use("/", fechaLanzamiento);
+router.use('/', buscar);
+router.use('/', rankingRouter);
 
 module.exports = router;
